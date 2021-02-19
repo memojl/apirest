6ba63b6583989d5c49a94b719550ad4d6e112d86
Memo Jimenez <memojl08@gmail.com>
2021-02-18 18:19:56 -0600

    update2 18022021

--- auth.php
+++ auth.php
@@ -2,12 +2,8 @@
 require_once 'clases/auth.class.php';
 require_once 'clases/respuestas.class.php';
 header("Access-Control-Allow-Origin: *");      
-<<<<<<< HEAD
-header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-=======
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
->>>>>>> 9a8104630198f11bc30450a604970c6c141c9dab
 
 $_auth = new auth;
 $_respuestas = new respuestas;
