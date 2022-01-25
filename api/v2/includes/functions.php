39cc906ba63e7eb69e45738b7890c39c5de8ca4d
memojl <memojl08@gmail.com>
2022-01-24 21:50:51 -0600

    update Identificador Apirest

--- api/v2/includes/functions.php
+++ api/v2/includes/functions.php
@@ -2,7 +2,19 @@
 //Campo ID
 function Identificador(){
 global $tabla;
-    switch ($tabla) {
+    switch (true) {
+        case ($tabla=='citas'):
+            $ID='CitaId';
+        break;
+        case ($tabla=='pacientes'):
+            $ID='PacienteId';
+        break;
+        case ($tabla=='usuarios'):
+            $ID='UsuarioId';
+        break;
+        case ($tabla=='usuarios_token'):
+            $ID='TokenId';
+        break;
         default:
             $ID='ID';
         break;
