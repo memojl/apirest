d0bf01f6fd3e042bcce0b4f65abadb6f5b324f47
memojl <memojl08@gmail.com>
2022-02-16 19:59:09 -0600

    update api 16.02.2022

--- api/login/index.php
+++ api/login/index.php
@@ -4,12 +4,11 @@ include_once '../v2/includes/conexion.php';
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
-header("Access-Control-Allow-Headers: X-Requested-With");
+header("Access-Control-Allow-Headers: Content-Type, X-Requested-With");
 //header('Content-Type: text/html; charset=utf-8');
 header('P3P: CP="IDC DSP COR CURa ADMa OUR IND PHY ONL COM STA"');
 
 //header('Content-Type: application/json');
-//echo $_SERVER['REQUEST_METHOD']; //Add /index.php to the URL
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $_POST = json_decode(file_get_contents('php://input'),true);
     login();
